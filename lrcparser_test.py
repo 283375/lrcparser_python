a1623b91dc6710b543e1674c5920f71b678e7b01
283375 <bilibili283375@163.com>
2023-01-14 11:10:45 +0800

    switch to unittest

--- lrcparser_test.py
+++ lrcparser_test.py
@@ -1,41 +1,46 @@
+import unittest
 import doctest
 from datetime import timedelta
 
 import lrcparser
 from lrcparser import LrcLine, LrcParser, TRANSLATION_DIVIDER
 
-test_LrcLine = LrcLine(
-    start_timedelta=timedelta(seconds=5, milliseconds=593),
-    text="This is a test line.",
-    offset_ms=50,
-    translations=["这是测试。"],
-)
 
-class TestLrcLine:
+class TestCase_LrcLine(unittest.TestCase):
+    test_LrcLine = LrcLine(
+        start_timedelta=timedelta(seconds=5, milliseconds=593),
+        text="This is a test line.",
+        offset_ms=50,
+        translations=["这是测试。"],
+    )
+
     def test_get_time(self):
-        assert test_LrcLine.get_time() == {
-            "minutes": 0,
-            "seconds": 5,
-            "microseconds": 593000,
-        }
+        self.assertDictEqual(
+            self.test_LrcLine.get_time(),
+            {
+                "minutes": 0,
+                "seconds": 5,
+                "microseconds": 593000,
+            },
+        )
 
     def test_to_str(self):
-        assert test_LrcLine.to_str() == "[00:05.59]This is a test line."
-        assert (
-            test_LrcLine.to_str(ms_digits=3, translations=True)
-            == f"[00:05.593]This is a test line.{TRANSLATION_DIVIDER}这是测试。"
+        self.assertEqual(self.test_LrcLine.to_str(), "[00:05.59]This is a test line.")
+        self.assertEqual(
+            self.test_LrcLine.to_str(ms_digits=3, translations=True),
+            f"[00:05.593]This is a test line.{TRANSLATION_DIVIDER}这是测试。",
         )
-        assert (
-            test_LrcLine.to_str(
+        self.assertEqual(
+            self.test_LrcLine.to_str(
                 ms_digits=5, translations=True, translation_divider=" /|\\"
-            )
-            == "[00:05.59300]This is a test line. /|\\这是测试。"
+            ),
+            "[00:05.59300]This is a test line. /|\\这是测试。",
         )
 
     def test_format_functions(self):
-        assert str(test_LrcLine) == test_LrcLine.to_str()
-        assert int(test_LrcLine) == 5593
-        assert float(test_LrcLine) == 5593.000
+        self.assertEqual(str(self.test_LrcLine), self.test_LrcLine.to_str())
+        self.assertEqual(int(self.test_LrcLine), 5593)
+        self.assertEqual(float(self.test_LrcLine), 5593.000)
 
 
 parser = LrcParser()
@@ -43,51 +48,66 @@ with open("example.lrc", "r", encoding="utf-8") as lrc_file:
     result = parser.parse(lrc_file.read())
 
 
-class TestParser:
+class TestCase_LrcParser(unittest.TestCase):
     def test_parse(self):
         expected_lrc_line = LrcLine(
             start_timedelta=timedelta(seconds=28, microseconds=330000),
             text="Line 4 with TRANSLATION! COOL!!!",
+            offset_ms=250,
         )
-        assert result["lrc_lines"][3] == expected_lrc_line
-        assert (
-            result["lrc_lines"][5].start_timedelta
-            == result["lrc_lines"][6].start_timedelta
+        self.assertEqual(result["lrc_lines"][3], expected_lrc_line)
+        self.assertEqual(
+            result["lrc_lines"][5].start_timedelta,
+            result["lrc_lines"][6].start_timedelta,
         )
-        assert result["attributes"] == [
-            {"name": "ti", "value": "test_lyric"},
-            {"name": "ar", "value": "283375"},
-            {"name": "al", "value": "TEST ~エラーを回避するための最良の方法~"},
-            {"name": "by", "value": "283375"},
-            {"name": "OFFset", "value": "250"},
-        ]
+        self.assertListEqual(
+            result["attributes"],
+            [
+                {"name": "ti", "value": "test_lyric"},
+                {"name": "ar", "value": "283375"},
+                {"name": "al", "value": "TEST ~エラーを回避するための最良の方法~"},
+                {"name": "by", "value": "283375"},
+                {"name": "OFFset", "value": "250"},
+            ],
+        )
 
     def test_offset(self):
         offset_lines = parser.apply_global_offset(
             result["lrc_lines"], result["global_offset"]
         )
-        assert offset_lines[0].offset_ms == 250
-        assert offset_lines[0].offset_timedelta == timedelta(
-            seconds=0, milliseconds=20 + 250
+        self.assertEqual(offset_lines[0].offset_ms, 250)
+        self.assertEqual(
+            offset_lines[0].offset_timedelta,
+            timedelta(seconds=0, milliseconds=20 + 250),
         )
 
     def test_find_duplicate(self):
         dups = parser.find_duplicate(result["lrc_lines"])
-        assert dups[0][0].text == "Line 4 with TRANSLATION! COOL!!!"
-        assert dups[0][1].start_timedelta == timedelta(seconds=28, microseconds=330000)
-        assert dups[1][0].text == "Sad because secs < 60"
+        self.assertEqual(dups[0][0].text, "Line 4 with TRANSLATION! COOL!!!")
+        self.assertEqual(
+            dups[0][1].start_timedelta, timedelta(seconds=28, microseconds=330000)
+        )
+        self.assertEqual(dups[1][0].text, "Sad because secs < 60")
 
     def test_combine_translation(self):
         translations = parser.combine_translation(result["lrc_lines"])
-        assert translations[0].text == "Line 4 with TRANSLATION! COOL!!!"
-        assert translations[0].translations == ["这行有翻译！真他妈的酷！！！"]
-        assert translations[1].text == "Sad because secs < 60"
-        assert translations[1].translations == [
-            "But we can change the rules :)",
-            "我只是来凑数的",
-        ]
+        self.assertEqual(translations[0].text, "Line 4 with TRANSLATION! COOL!!!")
+        self.assertEqual(translations[0].translations, ["这行有翻译！真他妈的酷！！！"])
+        self.assertEqual(translations[1].text, "Sad because secs < 60")
+        self.assertEqual(
+            translations[1].translations,
+            [
+                "But we can change the rules :)",
+                "我只是来凑数的",
+            ],
+        )
 
-class TestDocstring:
+
+class TestCase_Docstring(unittest.TestCase):
     def test_docstring(self):
         fail_count, test_count = doctest.testmod(lrcparser)
-        assert fail_count == 0
+        self.assertEqual(fail_count, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
